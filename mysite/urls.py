3da443dba3c4c1eedc9c9e33a56c3fdc05655187
takuya2816 <takuya.t0909r.kishiy.hack@gmail.com>
2021-03-06 10:50:52 +0900

    add html second

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))  # ''で始まるURLははblogアプリのurl.pyの引用
+    path('a', include('blog.urls'))  # ''で始まるURLははblogアプリのurl.pyの引用
 ]
