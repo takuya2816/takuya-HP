689a1417de38867a6cee876d2cebeddfe9e88cc9
takuya2816 <takuya.t0909r.kishiy.hack@gmail.com>
2021-03-06 11:04:05 +0900

    change setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*d+$o&dnwala%9n^v#+g410o_*rui)udt=vwak)7*i7ru=!_r1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
